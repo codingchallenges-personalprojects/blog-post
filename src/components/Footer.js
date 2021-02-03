ed8eedc3cff92f02469d2e7adf3825e229869807
mitra-tech <55147150+mitra-tech@users.noreply.github.com>
2021-02-03 11:47:53 -0700

    ready to deploy

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            &copy; <em>Mitra Davoudi</em>. Powered by
+            <a href="https://reduxblog.herokuapp.com/" target="_blank" title="Free Blog Post API"><em> Redux Blog Post API</em></a>
+        </div>
+    )
+}
+
+export default Footer;
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import 'font-awesome/css/font-awesome.min.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';import App from './components/App';
+import reduxThunk from 'redux-thunk';
+import reducers from './reducers';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware(reduxThunk)));
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'))
